a11e7e0c26700080b6f83cb5cbc68b1e7f2def39
无颜祖 <1465041302@qq.com>
2020-03-05 17:43:27 +0800

    add svg.vue

---
+++ src/components/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    style="display:block;width:330px;height:240px;background: url(../assets/images/log.svg) no-repeat;background-size: 100%"
+  ></div>
+
+  <!--<footer id="global-footer">
+				<div class="container">
+					<address>黒川温泉 旅館 山河／熊本県阿蘇郡南小国町大字満願寺6961-1<br><a href="tel:0967440906" class="no-pjax">TEL 0967-44-0906</a>／FAX 0967-44-0570</address>
+					<nav>
+						<ul>
+							<li><a href="/privacy/">プライバシーポリシー</a></li>
+							<li><a href="/sitepolicy/">サイトご利用案内</a></li>
+							<li><small>© 2013 Ryokan-Sanga.</small></li>
+						</ul>
+					</nav>
+					<div id="banner-area">
+						<ul>
+							<li id="banner-visit-kumamoto"><a href="http://visit-town.com/kumamoto/" target="_blank" class="no-pjax" title="熊本県の観光情報、オプショナルツアー VISIT熊本県"><img src="https://cdn.kumamoto.visit-town.com/assets/img/banner/banner120x60.jpg" alt="VISIT 熊本県"></a></li>
+						</ul>
+					</div>
+				<div id="lean_overlay"></div></div>
+  </footer>-->
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+@import "../assets/css/default.css";
+</style>
