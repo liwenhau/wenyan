90b1bbe989e478dc39a3c7f734a469a30891f534
无颜祖 <1465041302@qq.com>
2020-03-06 14:52:03 +0800

    优化首页

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,35 +1,145 @@
 <template>
-  <div
-    style="display:block;width:330px;height:240px;background: url(../assets/images/log.svg) no-repeat;background-size: 100%"
-  ></div>
+  <div id="wrapper" style="visibility: visible; opacity: 1;">
+    <div id="contents-wrapper">
+      <div id="pjax-container">
+        <section id="contents">
+          <div id="main">
+            <p id="catch" style="transition: none 0s ease 0s; opacity: 1; top: 1px;">
+              <svg
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                width="104px"
+                height="194px"
+                viewBox="0 0 104 194"
+                enable-background="new 0 0 104 194"
+                xml:space="preserve"
+              >
+                <image
+                  id="image0"
+                  width="104"
+                  height="194"
+                  x="0"
+                  y="0"
+                  href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGgAAADCCAAAAACA8Nj1AAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZ
+cwAADsMAAA7DAcdvqGQAAAAHdElNRQfkAwUVJTTi1TwfAAAEJUlEQVR42u2a4ZXkIAjH04mlWIql
+UAqlWAql5KJGxWR2gxO8e+8eftkkM8tvwL9I1G3/S20zkIEMZCADGchABjKQgQxkIAMZyEAG+ueg
+iBAASQ9EO9HNHIbtbB61QC6bixwd3MaaF3n1DIJiDlrIwnZpTkIShK7+8HLnt3uTkARiwNNcih65
+EeEh7lFEkqjOd9BOWF1yCVI/9yqgbKlazTCMkX0c6q94Ddp/NxO4Vt6BeCMMPhB/knQZlEERTjUg
+e4jaIAKmOWZZA8R+OF0Gav9IA+RiC9ptlKqCfBuKNFA8z9uwCQbSAwh6yuz5OtKYCjRURz29nNkn
+5/Fx3KjI27PO6AY966HyHB7sSEBNX5igh8adP5xjsQuXcfUVyLGOhpRJa09dQFEDVI0C1x2zrAdi
+BrcPHvnx9n3oGsjdQU+cKdBhMBcMYXfLQa4o/K461dCdudsFWgDiM8JQmLDvqKiOg7YfQKDlEZTL
+lr9DmjLYbA4aYmBjk85am2JJD1BRah5huYz5MrLC+/wAF4BgEMQ5V6GWGJCHrjbAZjtqgWKPUBlH
+l1iRAogKiHoNFGLBx/FL+BKUax+irVX2sfrJXVAE1aB1yxzk3k/lUJKAuxSnY65L2VYJFN3ZOR3E
+PfIq5VYpeCL3IFnm90Gh3HIfi1AYQfC+Uk1qgPtTbKCcW8M21HlfgQ4jeH9IDZSL8yBI349foI9v
+3L7+H6YhJCkaJEs0HxrWUGW1XKqiL0GfG9aLmppWgbrD7ja1rwGdSd2vB5WaBf4CCDaNiU/S9NaC
+NJqBDGQgAxnIQAb6qQESxf6SvA50FouwHFRX88NqUN0bE25UvgC1NQ3RRuULUN8K8Ys9qivSDpeC
+Koa/oK8AZSk4BzFi8K7cOB/aup2iR+C9v2z5SgeWGEQYPiDkA0u4Dws/Q4Qkwcsy+O25PZJ+B32O
+VxIARuqHJySkX0A++DuirnAyigtwyPBpxe4XEIwQD+PCEyEkvyg+Bv8xdH0bJ/RJKEcTH+1Ogfbo
+c3c0R2LrsqAL4g1H8U1PFGLQVRizE4Xco3HY4LLQ7e2wSdqpmsXMJVX8eo6YAyW1u2NOiOD76QN9
+UFou8xir9sIqEG7nSuO3+haCLunoC30LQbcc7sKk9GQgukDk5/deeLRW3jUvSM8Hfg3K5+pcK6xo
+PnYT8oZ6nY8AzI5ZKahNdv1cFa4AxXMFfTgAGRaAMO/bxMukNJMdhKDDkYY5qpQmQnVQ2i7MYzWf
+UOwJCRaAeq+wfpK7NJMZyvFb5FmCVoDyr+cHYodzv8ogdiB2CjM1HyUQOzcoe9GbBYXqER9GuABU
+N34v1d0CULiFbs2ATSrIVpPo3PHG7PLpqgmf5OWWd/lP6NUwRUDpv88UkCT/6jvQu2YgAxnIQAYy
+kIEMZCADGchABvpfQX8AJcVw2MLq4N4AAAAldEVYdGRhdGU6Y3JlYXRlADIwMjAtMDMtMDVUMTM6
+Mzc6NTIrMDg6MDCUo/ovAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIwLTAzLTA1VDEzOjM3OjUyKzA4
+OjAw5f5CkwAAAABJRU5ErkJggg=="
+                />
+              </svg>
+            </p>
+            <div class="container">
+              <ul id="navigation">
+                <li>
+                  <a href="/news/">
+                    <img src="../assets/images/nav_news.png" alt="お知らせ" />
+                  </a>
+                </li>
+                <!--
+                -->
+                <li>
+                  <a href="/about/" class="line">
+                    <img src="../assets/images/nav_about.png" alt="宿のご案内" />
+                  </a>
+                </li>
+                <!--
+                -->
+                <li>
+                  <a href="/reserve/">
+                    <img src="../assets//images/nav_reserve.png" alt="予約" />
+                  </a>
+                </li>
+                <!--
+                -->
+                <li>
+                  <a href="/access/">
+                    <img src="../assets//images/nav_access.png" alt="交通" />
+                  </a>
+                </li>
+                <!--
+                -->
+                <li>
+                  <a href="/blog/">
+                    <img src="../assets//images/nav_blog.png" alt="若女将の日記" />
+                  </a>
+                </li>
+              </ul>
+              <div id="lean_overlay"></div>
+            </div>
+          </div>
+          <nav id="sub-nav">
+            <ul>
+              <li id="english">
+                <a href="/en/" class="no-pjax">English</a>
+              </li>
+              <li id="facebook">
+                <a href="https://ja-jp.facebook.com/ryokan.sanga" target="_blank">Facebook</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </div>
+    </div>
 
-  <!--<footer id="global-footer">
-				<div class="container">
-					<address>黒川温泉 旅館 山河／熊本県阿蘇郡南小国町大字満願寺6961-1<br><a href="tel:0967440906" class="no-pjax">TEL 0967-44-0906</a>／FAX 0967-44-0570</address>
-					<nav>
-						<ul>
-							<li><a href="/privacy/">プライバシーポリシー</a></li>
-							<li><a href="/sitepolicy/">サイトご利用案内</a></li>
-							<li><small>© 2013 Ryokan-Sanga.</small></li>
-						</ul>
-					</nav>
-					<div id="banner-area">
-						<ul>
-							<li id="banner-visit-kumamoto"><a href="http://visit-town.com/kumamoto/" target="_blank" class="no-pjax" title="熊本県の観光情報、オプショナルツアー VISIT熊本県"><img src="https://cdn.kumamoto.visit-town.com/assets/img/banner/banner120x60.jpg" alt="VISIT 熊本県"></a></li>
-						</ul>
-					</div>
-				<div id="lean_overlay"></div></div>
-  </footer>-->
+    <div id="images" style="height:928px;">
+      <div id="catch" style="transition: none 0s ease 0s; opacity: 1; top: 1px;">
+        <!--<img src="https://www.sanga-ryokan.com/images/catch.png" alt="黒川のはずれ。針葉樹林の森の中、ひっそりと佇む宿。" />-->
+        <div id="wenyanoto" class="wenyan-fullpage animated bounce">
+          <div class="bracket left">『</div>
+          <div class="word lengthways" id="wenyan_text">{{wenyantext}}</div>
+          <div class="bracket right">』</div>
+          <div class="author" id="wenyan_author">—— 「一禅小和尚」</div>
+        </div>
+		<slide/>
+      </div>
+    </div>
+    <div id="indK" class="indicator" style="width: 951px; height: 30px;">
+      <span
+        style="background: url(&quot;https://www.sanga-ryokan.com/common/images/indicator_k.png&quot;) left top no-repeat;"
+      ></span>
+    </div>
+  </div>
 </template>
 <script>
+import slide from ".//slideshow ";
 export default {
   data() {
-    return {};
+    return {
+      wenyantext: "虽然想做一团火，却成了一块透视苦难的冰"
+    };
   },
-  methods: {}
+  methods: {},
+  components: {
+    slide
+  }
 };
 </script>
 
 <style scoped>
 @import "../assets/css/default.css";
+@import "../assets/css/init.css";
+@import "../assets/css/html5reset-1.6.1.css";
 </style>
---
+++ src/components/slideshow .vue
@@ -0,0 +1,51 @@
+<template>
+  <ul>
+    <li v-for="(item,index) in sildelist" v-bind:key="index">
+      <img :src="item.imgPath" alt />
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  mounted() {
+    this.imgHeight =
+      document.documentElement.clientHeight || document.body.clientHeight;
+    window.addEventListener("mousewheel", this.handleScroll, false);
+    window.addEventListener("DOMMouseScroll", this.handleScroll, false);
+  },
+  data() {
+    return {
+      sildelist: [
+        {
+          imgPath: require("../assets/images/sideimages/1.jpg")
+        },
+        {
+          imgPath: require("../assets/images/sideimages/2.jpg")
+        },
+        {
+          imgPath: require("../assets/images/sideimages/3.jpg")
+        },
+        {
+          imgPath: require("../assets/images/sideimages/4.jpg")
+        }
+      ],
+      imgHeight: ""
+    };
+  },
+  methods: {
+    handleScroll(e) {
+      var direction = e.deltaY > 0 ? "down" : "up"; //该语句可以用来判断滚轮是向上滑动还是向下
+
+      if (direction == "up") {
+          document.getElementById("images").getElementsByTagName("ul")[0].style.marginTop+=this.imgHeight;
+      }
+      console.log(direction);
+      console.log(this.imgHeight);
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
