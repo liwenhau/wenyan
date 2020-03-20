0e6cb3e92be7fd4e660c0ea71c624a6848ecdf4f
liwenhua <blake.li@headwin.com.cn>
2020-03-20 16:57:49 +0800

    add

--- src/components/slideshow .vue
+++ src/components/slideshow .vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="scorllwrapper" id="scorllwrapper">
+  <div ref="scorllwrapper" id="scorllwrapper" @scroll="fathScroll">
     <ul ref="ul">
       <li v-for="(item,index) in sildelist" v-bind:key="index">
         <img :src="item.imgPath" alt ref="img" />
@@ -9,9 +9,7 @@
 </template>
 <script>
 export default {
- 
   mounted() {
-   
     window.addEventListener("mousewheel", this.handleScroll, {
       passive: false
     });
@@ -48,11 +46,12 @@ export default {
       if (event.ctrlKey === true || event.metaKey) {
         event.preventDefault();
       }
-
+      console.log("触发");
       var direction = event.deltaY > 0 ? "down" : "up"; //该语句可以用来判断滚轮是向上滑动还是向下  scrollHeight:8920
       var scorllwrapperHeight = parseFloat(
         document.getElementById("scorllwrapper").style.height
       );
+
       if (direction == "up") {
         if (this.$refs.scorllwrapper.scrollTop == 0) {
           this.$refs.scorllwrapper.scrollTop =
@@ -68,7 +67,24 @@ export default {
           this.$refs.scorllwrapper.scrollTop = 0;
         }
       }
-        
+    },
+    fathScroll() {
+     /* const scorllwrapper = this.$refs.scorllwrapper;
+      var scrollTop = this.$refs.scorllwrapper.scrollTop;
+       console.log("外面"+scrollTop);
+
+       if(scrollTop>20){
+         console.log(scrollTop);
+         scrollTop-=20;
+           window.cancelAnimationFrame(timer);
+       }
+    
+      var timer = requestAnimationFrame(function() {
+        // 页面滚动的距离
+        //let speed = Math.floor(-scrollTop / 6);
+          
+        scorllwrapper.scrollTop=scrollTop+20;
+      });*/
     }
   }
 };
@@ -84,6 +100,7 @@ export default {
   overflow-y: scroll;
   overflow-x: hidden;
   scrollbar-width: none; /*隐藏滚动条兼容火狐*/
+  scroll-behavior: smooth;
 }
 #scorllwrapper::-webkit-scrollbar {
   display: none; /*隐藏滚动条兼容谷歌*/
